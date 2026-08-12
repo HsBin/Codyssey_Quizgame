02d57c4d2ab001b9c74d09ea112a3f37c231ed3f
장익빈 <wkddlrqls908@gmail.com>
2026-08-13 07:29:19 +0900

    feat: Quizgame 클래스 __init__, get_default_quizzes 함수구현

--- main.py
+++ main.py
@@ -27,3 +27,43 @@ class Quiz:
             "choices": self.choices,
             "answer": self.answer
         }
+
+#QuizGame을 진행하는 class
+class QuizGame:
+    def __init__(self):
+        self.quizzes = []
+        self.best_score = None
+        self.best_correct = 0
+        self.best_total = 0
+
+        self.load_state()
+	
+#기본 퀴즈 반환(주제: 게임)
+    def get_default_quizzes(self):
+        return [
+            Quiz(
+                "마인크래프트를 개발한 인물은 누구일까요?",
+                ["게이브 뉴웰", "마르쿠스 페르손", "토비 폭스", "시드 마이어"],
+                2
+            ),
+            Quiz(
+                "리그 오브 레전드를 개발한 회사는?",
+                ["Valve", "Blizzard", "Riot Games", "Nintendo"],
+                3
+            ),
+            Quiz(
+                "언리얼 엔진을 개발한 회사는?",
+                ["Epic Games", "Unity Technologies", "Valve", "Ubisoft"],
+                1
+            ),
+            Quiz(
+                "스타듀 밸리의 개발자는?",
+                ["Eric Barone", "Markus Persson", "Hideo Kojima", "Shigeru Miyamoto"],
+                1
+            ),
+            Quiz(
+                "젤다의 전설 시리즈를 제작한 게임 회사는?",
+                ["Sony", "Nintendo", "Microsoft", "Capcom"],
+                2
+            )
+        ]
