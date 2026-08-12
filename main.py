39f68b7a00be72f499c79e44949b75f0b35ad172
장익빈 <wkddlrqls908@gmail.com>
2026-08-13 07:27:18 +0900

    Feat: Quiz 클래스 구현

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#main.py
+import json
+import os
+import random
+
+STATE_FILE = "state.json"
+
+#Quiz 클래스, 퀴즈 1개의 역할
+class Quiz:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+
+    def display(self):
+        print("\n" + "-" * 40)
+        print(self.question)
+
+        for index, choice in enumerate(self.choices, start=1):
+            print(f"{index}. {choice}")
+
+    def check_answer(self, user_answer):
+        return user_answer == self.answer
+
+    def to_dict(self):
+        return {
+            "question": self.question,
+            "choices": self.choices,
+            "answer": self.answer
+        }
